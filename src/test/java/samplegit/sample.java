69a181db75d56af1175484df907b6c7fdd17028e
sridevi <sridevinakshatra0718@gmail.com>
2025-01-20 21:28:38 +0530

    firstcommit

---
+++ src/test/java/samplegit/sample.java
@@ -0,0 +1,15 @@
+package samplegit;
+
+public class sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 5;
+		int b =10;
+		int c = a+b;
+		System.out.println(c);
+
+	}
+
+}
